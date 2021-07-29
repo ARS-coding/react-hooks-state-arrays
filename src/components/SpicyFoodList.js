6518628e74740bacd1af4026a5de3a4a7dc2b64a
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-29 16:30:59 +0300

    Display, remove and increase the heat level of foods.

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,13 +6,45 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    if(newFood) {
+      setFoods([...foods, newFood]);
+    } else {
+      console.log("There's no new food left!");
+    }
+  }
+
+  function handleRemove(event) {
+    setFoods(foods.filter(food => food.id != event.target.id))
+  }
+
+  function handleIncreaseHeat(event) {
+    // const selectedFood = foods.filter(foodObj => foodObj.id == event.target.id)[0];
+    // console.log(selectedFood)
+    setFoods(foods.map(foodObj => {
+      if(foodObj.id == event.target.id) {
+        return { ...foodObj, heatLevel: foodObj.heatLevel + 1 };
+      }
+      return foodObj;
+    }))
   }
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>
+        {
+          foods.map(
+            foodObj => {
+              return (
+                <li key={foodObj.id}>
+                  Name: {foodObj.name} | Cuisine: {foodObj.cuisine} | Heat Level: {foodObj.heatLevel}
+                  <button id={foodObj.id} onClick={handleRemove}>X</button>
+                  <button id={foodObj.id} onClick={handleIncreaseHeat}>Increase Heat</button>
+                </li>
+              )
+          })
+        }
+      </ul>
     </div>
   );
 }
